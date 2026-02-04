7a5dead1a8b2551e6db6223653772b77c3c78c78
Nekrosies <z2529323861@163.com>
2026-02-04 11:40:08 +0800

    fix: 简化物品接口

--- server/src/gameplay/entity/item/BaseItem.ts
+++ server/src/gameplay/entity/item/BaseItem.ts
@@ -2,10 +2,4 @@ export interface IItemData {
   id: string;
   name: string;
   discription: string;
-  fryable: boolean;
-  afterFry: string;
-  boilable: boolean;
-  afterBoil: string;
-  cuttable: boolean;
-  afterCut: string;
 }
