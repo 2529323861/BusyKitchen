47bdf7bd523821374093887915ed03a6b9f39433
Nekrosies <z2529323861@163.com>
2026-02-05 17:34:29 +0800

    feat: JsonDataMgr实现煮菜表和煎炸表的读取与查找

--- server/src/gameplay/mgr/JsonDataMgr.ts
+++ server/src/gameplay/mgr/JsonDataMgr.ts
@@ -1,11 +1,20 @@
 import { Singleton } from '../../framework/common/Singleton';
-import type { IItemData, ICuttingTableData } from '../jsonData/DataInterface';
+import type {
+  IItemData,
+  ICuttingTableData,
+  IBoilTableData,
+  IFryTableData,
+} from '../jsonData/DataInterface';
 import { item } from '../jsonData/item.json';
 import { cuttingTable } from '../jsonData/cuttingTable.json';
+import { boilTable } from '../jsonData/BoilTable.json';
+import { fryTable } from '../jsonData/fryTable.json';
 
 export class JsonDataMgr extends Singleton<JsonDataMgr>() {
   private _itemMap: Map<string, IItemData> = new Map();
   private _cuttingTableArray: Array<ICuttingTableData> = [];
+  private _boilTableArray: Array<IBoilTableData> = [];
+  private _fryTableArray: Array<IFryTableData> = [];
 
   constructor() {
     super();
@@ -20,6 +29,16 @@ export class JsonDataMgr extends Singleton<JsonDataMgr>() {
     cuttingTable.forEach((value) => {
       this._cuttingTableArray.push(value);
     });
+
+    /** 读取煮菜表数据 */
+    boilTable.forEach((value) => {
+      this._boilTableArray.push(value);
+    });
+
+    /** 读取煎炸表数据 */
+    fryTable.forEach((value) => {
+      this._fryTableArray.push(value);
+    });
   }
 
   /** 从物品数据中获取数据 */
@@ -30,6 +49,16 @@ export class JsonDataMgr extends Singleton<JsonDataMgr>() {
   /** 检索切菜表配方 */
   public searchCuttingTable(material: string): ICuttingTableData | undefined {
     return this._cuttingTableArray.find((value) => value.material === material);
+  }
+
+  /** 检索水煮表配方 */
+  public searchBoilTable(material: string): IBoilTableData | undefined {
+    return this._boilTableArray.find((value) => value.material === material);
+  }
+
+  /** 检索切菜表配方 */
+  public searchFryTable(material: string): ICuttingTableData | undefined {
+    return this._fryTableArray.find((value) => value.material === material);
   }
 
   public start(): void {}
