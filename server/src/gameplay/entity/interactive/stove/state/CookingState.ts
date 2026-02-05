6d494c8e71657633be232481c7bf019d2cd69d69
Nekrosies <z2529323861@163.com>
2026-02-05 18:05:18 +0800

    fix: 5种状态追加构造函数绑定控制器，构建fryStove的抽象基类Stove用于继承

--- server/src/gameplay/entity/interactive/stove/state/CookingState.ts
+++ server/src/gameplay/entity/interactive/stove/state/CookingState.ts
@@ -1,8 +1,14 @@
 import type { IState } from '../../../../../framework/common/StateMachine';
 import { StoveState } from '../../../../const/stateConst';
+import type { Stove } from '../Stove';
 
 export class CookingState implements IState {
   name: string = StoveState.CookingState;
+  /** 控制器引用 */
+  private _interactive: Stove;
+  constructor(controller: Stove) {
+    this._interactive = controller;
+  }
   onEnter(prevState: string): void {}
   onUpdate(deltaTime: number): void {}
   onExit(nextState: string): void {}
