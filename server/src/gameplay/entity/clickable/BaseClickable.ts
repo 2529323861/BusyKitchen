a22886b4c498c2dd696125d8578a88aed63f5955
Nekrosies <z2529323861@163.com>
2026-02-05 12:58:24 +0800

    feat: 实现可点击实体基类

---
+++ server/src/gameplay/entity/clickable/BaseClickable.ts
@@ -0,0 +1,27 @@
+import type { StateMachine } from '../../../framework/common/StateMachine';
+
+export class BaseClickable {
+  protected _entity: GameEntity;
+
+  public _stateMachine?: StateMachine | null;
+
+  /** 绑定场景中的可点击实体 */
+  constructor(entity: GameEntity) {
+    this._entity = entity;
+  }
+
+  /** 初始化接口 */
+  public init(): void {}
+
+  /** 事件绑定 */
+  public bindevent(): void {}
+
+  /** 启动接口 */
+  public start(): void {}
+
+  /** 更新接口 */
+  public update(delta: number): void {}
+
+  /** 销毁接口 */
+  public destory(): void {}
+}
