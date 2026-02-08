b049bb16688caaa5616bbe2b85f07b59b8f6c499
Nekrosies <z2529323861@163.com>
2026-02-08 13:46:45 +0800

    feat: 添加逻辑屏幕基类、添加UI组件基类

---
+++ client/src/gameplay/entity/UIComponent/BaseUIComponent.ts
@@ -0,0 +1,6 @@
+export class BaseUIComponent {
+  public init(): void {}
+  public start(): void {}
+  public update(delta: number): void {}
+  public destory(): void {}
+}
---
+++ client/src/gameplay/entity/logicScreen/BaseLogicScreen.ts
@@ -0,0 +1,42 @@
+import type { BaseUIComponent } from '../UIComponent/BaseUIComponent';
+
+export class BaseLogicScreen {
+  /** 屏幕内组件 */
+  protected _componentMap: Map<string, BaseUIComponent> = new Map();
+  constructor() {}
+  public init(): void {
+    this._componentMap.forEach((value) => {
+      value.init();
+    });
+  }
+  /** 注册组件 */
+  public addComponent(token: string, component: BaseUIComponent): void {
+    this._componentMap.set(token, component);
+  }
+  /** 移除组件 */
+  public removeComponent(token: string): void {
+    this._componentMap.delete(token);
+  }
+  /** 获取组件 */
+  public getComponent(token: string): BaseUIComponent | undefined {
+    return this._componentMap.get(token);
+  }
+
+  public start(): void {
+    this._componentMap.forEach((value) => {
+      value.start();
+    });
+  }
+  public update(delta: number): void {
+    console.log('(client): GameLogicScreen update');
+    this._componentMap.forEach((value) => {
+      value.update(delta);
+    });
+  }
+  public destory(): void {
+    console.log('(client): GameLogicScreen start');
+    this._componentMap.forEach((value) => {
+      value.start();
+    });
+  }
+}
