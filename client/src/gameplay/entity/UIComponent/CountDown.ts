5368714b3834aaf08aa9121d1f681b5cbf169987
Nekrosies <z2529323861@163.com>
2026-02-17 12:20:00 +0800

    feat: 倒计时CountDown新增设置时间接口

--- client/src/gameplay/entity/UIComponent/CountDown.ts
+++ client/src/gameplay/entity/UIComponent/CountDown.ts
@@ -2,7 +2,7 @@ import { BaseUIComponent } from './BaseUIComponent';
 
 export class CountDown extends BaseUIComponent {
   private _countDown: UiText;
-  public time: number = 0;
+  private time: number = 0;
   constructor(countDown: UiText) {
     super();
     this._countDown = countDown;
@@ -10,5 +10,8 @@ export class CountDown extends BaseUIComponent {
   public update(delta: number): void {
     this.time -= delta;
     this._countDown.textContent = Math.round(this.time / 1000).toString();
+  }
+  public setTime(time: number): void {
+    this.time = time;
   }
 }
